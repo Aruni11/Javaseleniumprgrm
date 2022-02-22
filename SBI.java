b3ec89e96c0d85b231a6a6b0c44107cab066b88a
Aruni11 <99694961+Aruni11@users.noreply.github.com>
2022-02-22 07:38:46 -0800

    Add files via upload

---
+++ SBI.java
@@ -0,0 +1,10 @@
+
+
+public abstract class SBI {
+	public void ItLoan() {
+		System.out.println("IT Loan");
+	}
+	public abstract void bankBalance();
+
+	public abstract void  maximumLoanAmount();
+}
